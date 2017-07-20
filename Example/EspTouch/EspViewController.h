2b432cc49be8dddcafabec08f66199680f25710a
liubiqu <liubiqu@qq.com>
2017-07-20 08:57:16 +0800

    rename

--- Example/EspTouch/EspViewController.h
+++ Example/EspTouch/EspViewController.h
@@ -1,13 +1,14 @@
 //
-//  EspViewController.h
-//  EspTouch
+//  ESPViewController.h
+//  EspTouchDemo
 //
-//  Created by liubiqu on 07/20/2017.
-//  Copyright (c) 2017 liubiqu. All rights reserved.
+//  Created by 白 桦 on 3/23/15.
+//  Copyright (c) 2015 白 桦. All rights reserved.
 //
 
-@import UIKit;
+#import <UIKit/UIKit.h>
 
-@interface EspViewController : UIViewController
-
+@interface ESPViewController : UIViewController<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *ssidLabel;
+@property (strong, nonatomic) NSString *bssid;
 @end
